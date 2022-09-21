9862d76463d99ba7679cb4d1ef8135180d202d35
ludovic guadagnin <guadagnin.ludovic@gmail.com>
2022-09-21 16:51:46 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning up database..."
+Bookmark.destroy_all
+Movie.destroy_all
+puts "Database cleaned"
+
+require 'open-uri'
+require 'json'
+
+url = 'https://tmdb.lewagon.com/movie/popular'
+json = URI.open(url).read
+data = JSON.parse(json)
+
+data['results'].first(30).each do |movie|
+  Movie.create(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+end
+puts "Movies created"
